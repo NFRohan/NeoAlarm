0b9eb422869c0b51eec47e6cd39f02c96c00826b
NFRohan <nfrohan110@gmail.com>
2026-05-19 01:03:42 +0600

    Prepare v1.0.4 release

--- lib/src/core/app/app_metadata.dart
+++ lib/src/core/app/app_metadata.dart
@@ -2,8 +2,8 @@ class AppMetadata {
   const AppMetadata._();
 
   static const name = 'NeoAlarm';
-  static const version = '1.0.1';
-  static const buildNumber = '2';
+  static const version = '1.0.4';
+  static const buildNumber = '4';
   static const repositoryUrl = 'https://github.com/NFRohan/NeoAlarm';
   static const userAgent = '$name/$version (+$repositoryUrl)';
 }
